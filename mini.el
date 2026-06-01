7289b1d61ffa23b4c30fc8837b309f569d6bcbe7
Alex K Hon <alex@arcturus>
2026-06-01 16:08:26 -0400

    simplified start up

--- mini.el
+++ mini.el
@@ -7,21 +7,10 @@
 ;; Unclutter startup
 ;; ===========================================================================
 (setq inhibit-startup-message t)
-(menu-bar-mode -1)
 
-;; using emacs --daemon, these might need to be here
-(tool-bar-mode -1)
+(menu-bar-mode   -1)
+(tool-bar-mode   -1)
 (scroll-bar-mode -1)
-
-(if (display-graphic-p)
-    (progn
-      (tool-bar-mode -1)
-      (scroll-bar-mode -1))
-  nil
-  )
-
-;; load a color that
-
 
 ;; ===========================================================================
 ;; Package manager: straight.el
