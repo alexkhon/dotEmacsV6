042e31fc1d0888d8ba92718996299455090e2949
Alex K Hon <alex@arcturus>
2026-06-18 14:58:30 -0400

    added setupV6.sh

---
+++ setV6.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# remove the links in $HOME
+rm -f $HOME/.emacs
+rm -f $HOME/.emacs.d
+
+# clean out emacsDir
+rm -rf $HOME/Config/dotEmacsV6/emacsDir
+mkdir  $HOME/Config/dotEmacsV6/emacsDir
+
+# copy early-init.el to the emacs dir
+cp $HOME/Config/dotEmacsV6/early-init.el $HOME/Config/dotEmacsV6/emacsDir/early-init.el
+
+# add the links to $HOME
+ln -s $HOME/Config/dotEmacsV6/init.el  $HOME/.emacs
+ln -s $HOME/Config/dotEmacsV6/emacsDir  $HOME/.emacs.d
