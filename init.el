87db5a7a12fc369e505f38cdb4fb577f99e0714a
Alex K Hon <alex@alexs-mbp.mynetworksettings.com>
2026-06-19 22:02:10 -0400

    changed ibuffer.org from dotEmacsV5/babel to dotEmacsV6/babel

--- init.el
+++ init.el
@@ -178,7 +178,7 @@
 ;; ibuffer
 ;; ===========================================================================
 
-(org-babel-load-file (expand-file-name "~/Config/dotEmacsV5/babel/ibuffer.org"))
+(org-babel-load-file (expand-file-name "~/Config/dotEmacsV6/babel/ibuffer.org"))
 
 ;; ===========================================================================
 ;; minibuffer packages
