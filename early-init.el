675322c35a4c48d6e91cdf0fc4009acce8fa1d7c
Alex K Hon <alex.k.hon@outlook.com>
2026-09-25 17:59:01 -0400

    set lexical binding cookie

--- early-init.el
+++ early-init.el
@@ -1,3 +1,5 @@
+;;; -*- lexical-binding: t -*-
+
 ;; https://jeffkreeftmeijer.com/emacs-straight-use-package/
 ;; The ~/.emacs.d/early-init.el file disables package.el to disable
 ;; its auto-loading, causing all packages to be loaded through
